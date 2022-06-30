f149117a0bec67170960e6fc07b94daa0373df93
Andres Soca <lumen.development01@gmail.com>
2022-06-30 12:12:17 -0400

    dragons-spec passed and completed

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,26 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name
+    this.color = color
+  }
 
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons) {
+    const dragonNames = dragons.map((dragon) => (
+      dragon.name
+    ));
+
+    return dragonNames
+  }
+}
+
+
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
